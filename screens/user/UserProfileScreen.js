e3b7aefedf999318943b5855ede9a018ed6c0b0c
Saad Hmed <saad.ahmed2696@gmail.com>
2020-12-09 15:47:37 +0500

    Adding profile info

--- screens/user/UserProfileScreen.js
+++ screens/user/UserProfileScreen.js
@@ -1,24 +1,59 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text, ActivityIndicator } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../../components/UI/HeaderButton";
 import ListItemSeparator from "../../components/UI/ListItemSeparator";
 import Colors from "../../constants/Colors";
+import { db } from "../../firebase/Firebase";
+import { useSelector, useDispatch } from "react-redux";
+import { isLoading } from "expo-font";
 
 const UserProfileScreen = (props) => {
+  const ReduxCurrentUser = useSelector((state) => state.auth.userId);
+  const [loading, setLoading] = useState(false);
+  const [Profile, setProfile] = useState({});
+  useEffect(() => {
+    setLoading(true);
+    getProfile();
+    return () => {
+      getProfile();
+    };
+  }, []);
+  const getProfile = async () => {
+    setLoading(true);
+    db.collection("app-users")
+      .doc(ReduxCurrentUser)
+      .onSnapshot((snap) => {
+        setProfile({
+          name: snap.data().UserName,
+          address: snap.data().CurrentAddress,
+          email: snap.data().UserEmail,
+          phnumber: snap.data().phnumber,
+          timestamp: snap.data().timestamp,
+        });
+        setLoading(false);
+      });
+
+    console.log("here=====>", Profile);
+  };
   return (
     <View style={styles.screen}>
-      <Text style={styles.info}>CONTACT INFORMATION</Text>
-      <Text style={styles.data}>User Name {props.userName} </Text>
-      <ListItemSeparator />
-      <Text style={styles.data}>Address</Text>
-      <ListItemSeparator />
-      <Text style={styles.data}>Email Address </Text>
-      <ListItemSeparator />
-      <Text style={styles.data}>Mobile Number </Text>
-      <ListItemSeparator />
-      <Text style={styles.data}>joined on </Text>
-      <ListItemSeparator />
+      {loading === true ? (
+        <ActivityIndicator size="large" color={Colors.primary} />
+      ) : (
+        <View>
+          <Text style={styles.info}>YOUR PROFILE</Text>
+          <Text style={styles.data}>Name: {Profile.name}</Text>
+          <ListItemSeparator />
+          <Text style={styles.data}>Address: {Profile.address}</Text>
+          <ListItemSeparator />
+          <Text style={styles.data}>Email Address: {Profile.email} </Text>
+          <ListItemSeparator />
+          <Text style={styles.data}>Mobile Number: {Profile.phnumber}</Text>
+          <ListItemSeparator />
+          <ListItemSeparator />
+        </View>
+      )}
     </View>
   );
 };
