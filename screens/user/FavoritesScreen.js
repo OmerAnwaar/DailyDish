a7ca2c2af232cc0cf0eece7cd1219b856659d133
OmerAnwaar <31045808+OmerAnwaar@users.noreply.github.com>
2020-12-29 02:54:52 +0500

    tab Navigatin added

---
+++ screens/user/FavoritesScreen.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { useSelector } from "react-redux";
+
+import HeaderButton from "../../components/UI/HeaderButton";
+
+const FavoritesScreen = (props) => {
+  return (
+    <View style={styles.content}>
+      <Text>No favorite meals found. Start adding some!</Text>
+    </View>
+  );
+};
+
+FavoritesScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: "Your Favorites",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default FavoritesScreen;
